98b163fa0c1921d99ba5a89f5c1dce06501e427b
a-r-t <alexthimineur@gmail.com>
2022-12-07 14:50:36 -0500

    projectz finished

--- src/pages/semesters/fall2022/project-z/ProjectZ.vue
+++ src/pages/semesters/fall2022/project-z/ProjectZ.vue
@@ -30,21 +30,46 @@
         img(:src="require('./assets/co-op.png')")
         p.asset-description Co-op
       h3 Instructions
-      p The goal of each level is to find the key, and then use that key to unlock the animal from the cage.
-      p If an enemy hits the player, they will take damage. Take enough damage and its game over.
+      p Your goal is to survive as long as possible. 
+        | You want to do this by running around the map and trying to eliminate all the zombies. 
+        | The longer you last, the harder it gets! When you survive another wave or eliminate a zombie, you get money. 
+        | You can then use this money to buy different weapons or more ammo from the NPCs scattered around the map. 
+        | Good luck finding them! 
+      p For Co-op, you are working together! Be friends and communicate! Keep in mind that you share money, hearts, and ammunition!
+      p Alex Controls (single player or left player in co-op)
+      ul
+        li WALK UP KEY = W
+        li WALK LEFT KEY = A
+        li WALK DOWN KEY = S 
+        li WALK RIGHT KEY = D
+        li INTERACT KEY = SPACE (can be used to talk to NPCs)
+        li SHOOT KEY = F
+      p Falex Controls (right player in co-op)
+      ul
+        li WALK UP KEY = ARROW KEY UP
+        li WALK LEFT KEY = ARROW KEY LEFT
+        li WALK DOWN KEY = ARROW KEY DOWN
+        li WALK RIGHT KEY = ARROW KEY RIGHT
+        li INTERACT KEY = SPACE (can be used to talk to NPCs)
+        li SHOOT KEY = ENTER
       h3 Notes
-      p Collecting all of the coins in every level will give you a special reward at the end party screen (hit the disco ball)!
+      p The goal of the game is to last as long as possible. You only get one extra health and one nuke (kills all zombies currently on screen). Use them wisely!
+      p For Co-op, remember you are working TOGETHER! Have fun and enjoy playing with a friend!!!!!
+      p Known bugs: 
+      ul
+        li Voice lines always play even if you skip through NPC text.
+        li In co-op, when interacting with an NPC, the textbox will pop up on both player's screens instead of just the player that did the interacting.
       h3 Videos
       .video
         video(:src="require('./assets/single-player.mp4')" controls preload="metadata")
         p.asset-description Single-Player Gameplay
       h3 Downloads
       .download-container
-        a(href="https://drive.google.com/uc?export=download&id=1crSxO2C24-0mCX-xPFIAgDQ5V76ztXA0" download target="_blank") Download JAR File (must have Java 1.8 or greater installed)
+        a(href="https://drive.google.com/uc?export=download&id=1OwfyBg3JmUJwAO1b5rR6IIXqZrmzg5LV" download target="_blank") Download JAR File (must have Java 1.8 or greater installed)
       .download-container
-        a(href="https://drive.google.com/uc?export=download&id=1p2ImKstl6pikelwc7YOB9Z8yVvvpvOgk" download target="_blank") Download EXE File (Windows Only)
+        a(href="https://drive.google.com/uc?export=download&id=1oUYSADH5B-SuMtgQc4zgf2PLMM-TGgu6" download target="_blank") Download EXE File (Windows Only)
       .download-container
-        a(href="https://drive.google.com/uc?export=download&id=1p2ImKstl6pikelwc7YOB9Z8yVvvpvOgk" download target="_blank") Download APP File (MacOS Only)
+        a(href="https://drive.google.com/uc?export=download&id=1z8LNpE_LH74579daJ0xAWaPchj5nsKA1" download target="_blank") Download APP File (MacOS Only -- Unzip file first)
 </template>
 
 <script>

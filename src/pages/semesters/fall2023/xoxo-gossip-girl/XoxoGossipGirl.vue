d3f90cf3058422e79956e3360c9786adbc810a76
a-r-t <alexthimineur@gmail.com>
2023-12-03 14:58:42 -0500

    fix typo

--- src/pages/semesters/fall2023/xoxo-gossip-girl/XoxoGossipGirl.vue
+++ src/pages/semesters/fall2023/xoxo-gossip-girl/XoxoGossipGirl.vue
@@ -15,8 +15,8 @@
       p Hope you're ready for a scavenger hunt!
       p In this game, you will need to explore several worlds, collect items, and fight enemies in order to survive.
       p And for those of you that are fans of 
-        i Gossip Girl, 
-        can you spot all of the references spread throughout the game?
+        i Gossip Girl
+        span , can you spot all of the references spread throughout the game?
     template(v-slot:instructions)
       p N/A
     template(v-slot:notes)

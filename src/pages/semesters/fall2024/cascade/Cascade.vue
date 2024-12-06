24753ea35afe838faa2c5ce5f59663ab3c2f0357
a-r-t <alexthimineur@gmail.com>
2024-12-06 12:05:23 -0500

    most of cascade

--- src/pages/semesters/fall2024/cascade/Cascade.vue
+++ src/pages/semesters/fall2024/cascade/Cascade.vue
@@ -14,9 +14,25 @@
       template(v-slot:about)
         
       template(v-slot:instructions)
-        
+        p Your goal is to traverse the lands of Thimineur to take down the evil King John Kingsley (who has previously kicked your butt). 
+          |You need to gain your strength and beat his minions first, though, so do quests, fight enemies, and buy some good 'ol potions from your friendly neighborhood insect potion dealer using the coins you gain to give yourself the strength to fight Kingsley!
+
+        p Use WASD to move the player and to highlight options in menus and text boxes. 
+          | Use E to interact with NPCs and items, and select options in menus.
+
+        P Press I to open and close your inventory. 
+          | Press M to open the map.
       template(v-slot:notes)
-        
+        p Currently, there are a few times where you “get” an item that you don't actually get. 
+          | Those are usually needed for other quests, but don't show up in the inventory since they don't count as items - other than that, they're currently just a bit of flavor.
+
+        p There are a few minor bugs in the game, but nothing game-breaking. 
+          | If you encounter something game-breaking, consider not encountering that again.
+
+        p The inventory currently just shows what you have, and can't really be interacted with. 
+          | There aren't yet enough items to fill your inventory, so don't worry about running out of space.
+
+        P The map animation is pretty low FPS due to limitations with Java. It's not a you thing.
   </template>
   
   <script>
@@ -42,13 +58,21 @@
           'Andrew Labbe',
           'Garrett Goldberg'
         ],
-        heroScreenshotFilePath: undefined,
+        heroScreenshotFilePath: require('./assets/titlescreen.png'),
         genre: 'RPG',
         screenshots: [
-          
+          {
+            filePath: require('./assets/gameplay1.png')
+          },
+          {
+            filePath: require('./assets/gameplay2.png')
+          }
         ],
-        videos: [
-
+        videos: [        
+          // {
+          //   filePath: require('./assets/game-video.mp4'),
+          //   description: 'Full Game Playthrough'
+          // }
         ],
         downloads: [
           {
